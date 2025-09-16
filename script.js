c35d627f71f35d14f87f4c60e2b79955d0fdfa80
hodniukyan <hodniukyan@gmail.com>
2025-09-16 13:17:26 -0400

    Primeiro

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".section-azul, .section-laranja");
+
+  const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("section-show");
+        obs.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+
+  sections.forEach(section => observer.observe(section));
+});
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
